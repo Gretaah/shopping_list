298220d9fc7039648a16a8b05013357b78f1664f
Gretaah <fluganisoppan@gmailcom>
2020-11-09 21:33:55 +0100

    Removed edit button and updated html pattern to allow spaces

--- assets/js/list.js
+++ assets/js/list.js
@@ -12,13 +12,9 @@ shoppingList.addEventListener("submit", function (e) {
 const newListitem = (userInput) => {
     const newListItem = document.createElement("li");
     const delBtn = document.createElement("BUTTON");
-    const editBtn = document.createElement("BUTTON");
     delBtn.innerHTML = "Delete";
     delBtn.className = "delBtn";
-    editBtn.innerHTML = "Edit";
-    editBtn.className = "editBtn"
     newListItem.append(delBtn);
-    newListItem.append(editBtn);
     newListItem.append(`${userInput}`);
     listItems.append(newListItem);
 }

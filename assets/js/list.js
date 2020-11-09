40e0c589f5e23e2c9118f8bc9c6f47c791849b6d
Gretaah <fluganisoppan@gmailcom>
2020-11-09 17:24:13 +0100

    Added jquery and functionality to delete button

--- assets/js/list.js
+++ assets/js/list.js
@@ -1,6 +1,7 @@
 const shoppingList = document.querySelector("#shoppingList");
 const listItems = document.querySelector("#listItems");
 
+
 shoppingList.addEventListener("submit", function (e) {
     e.preventDefault();
     const userInput = this.elements.userInput;
@@ -13,9 +14,15 @@ const newListitem = (userInput) => {
     const delBtn = document.createElement("BUTTON");
     const editBtn = document.createElement("BUTTON");
     delBtn.innerHTML = "Delete";
+    delBtn.className = "delBtn";
     editBtn.innerHTML = "Edit";
+    editBtn.className = "editBtn"
     newListItem.append(delBtn);
     newListItem.append(editBtn);
     newListItem.append(`${userInput}`);
     listItems.append(newListItem);
 }
+
+$(document).on("click", ".delBtn", function () {
+    $(this).parent().remove();
+});
